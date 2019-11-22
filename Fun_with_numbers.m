a0abd86eff6974083a25edb757551b22d8b99e33
Prash Ganesan <43354782+cardyfib@users.noreply.github.com>
2019-11-21 23:34:44 -0800

    Update Fun_with_numbers.m

--- Fun_with_numbers.m
+++ Fun_with_numbers.m
@@ -4,27 +4,23 @@
 % different digit places and different digit numbers such as 4 digit and 5
 % digit numbers.
 
-% Copyright (C) 2017 Prasanth Ganesan - All Rights Reserved
-% Unauthorized copying of this file, via any medium is strictly prohibited
-% For permissions for third parties to view and/or reuse, contact Prasanth Ganesan
-
-% Written by Prasanth Ganesan <pganesan2017@fau.edu>, created on 03/22/2017
-% Last updated on 03/22/2017
+% Written by Prasanth "Prash" Ganesan <prasganesan.pg@gmail.com>
+% created on 03/22/2017
 
 x=[1:9];
 solutions1 = [];
 solutions2 = [];
 solutions3 = [];
 for i = 1:length(x)
-yfull=(1/3)*str2double(strcat(num2str(x(i)),num2str(x(i)),num2str(x(i))));
-ysep = str2double(regexp(num2str(yfull),'\d','match'));
-if ysep(1) == x(i)
-    solutions1 = [solutions1 yfull];
-elseif ysep(2) == x(i)
-    solutions2 = [solutions2 yfull];
-elseif length(ysep)>2
-    if ysep(3) == x(i)
-    solutions3 = [solutions3 yfull];
+    yfull=(1/3)*str2double(strcat(num2str(x(i)),num2str(x(i)),num2str(x(i))));
+    ysep = str2double(regexp(num2str(yfull),'\d','match'));
+    if ysep(1) == x(i)
+        solutions1 = [solutions1 yfull];
+    elseif ysep(2) == x(i)
+        solutions2 = [solutions2 yfull];
+    elseif length(ysep)>2
+        if ysep(3) == x(i)
+            solutions3 = [solutions3 yfull];
+        end
     end
-end
 end
