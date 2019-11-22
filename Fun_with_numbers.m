4c14fb69ef5e6d3f31e81da481ede337ce619d29
Prash Ganesan <43354782+cardyfib@users.noreply.github.com>
2019-11-21 23:31:53 -0800

    Add files via upload

---
+++ Fun_with_numbers.m
@@ -0,0 +1,30 @@
+% Just for fun: Find all three digit numbers that when added to the same
+% three times, gives a three digit number of all the digits of the middle
+% of the original number. i.e. xyz + xyz + xyz = yyy . You can continue for
+% different digit places and different digit numbers such as 4 digit and 5
+% digit numbers.
+
+% Copyright (C) 2017 Prasanth Ganesan - All Rights Reserved
+% Unauthorized copying of this file, via any medium is strictly prohibited
+% For permissions for third parties to view and/or reuse, contact Prasanth Ganesan
+
+% Written by Prasanth Ganesan <pganesan2017@fau.edu>, created on 03/22/2017
+% Last updated on 03/22/2017
+
+x=[1:9];
+solutions1 = [];
+solutions2 = [];
+solutions3 = [];
+for i = 1:length(x)
+yfull=(1/3)*str2double(strcat(num2str(x(i)),num2str(x(i)),num2str(x(i))));
+ysep = str2double(regexp(num2str(yfull),'\d','match'));
+if ysep(1) == x(i)
+    solutions1 = [solutions1 yfull];
+elseif ysep(2) == x(i)
+    solutions2 = [solutions2 yfull];
+elseif length(ysep)>2
+    if ysep(3) == x(i)
+    solutions3 = [solutions3 yfull];
+    end
+end
+end
---
+++ Fun_with_numbers_KaprekarNumber.m
@@ -0,0 +1,37 @@
+% Kaprekar's constant = 6174 for 4 digit. Most of the 4 digit numbers if you split the
+% digits, then sort them in ascending and descending order, then subtract
+% the large from the small number, then split the result, then repeat
+% the same process for the split digits, you will arrive at the constant
+% 6174. Run this program and try it! You can also try it for three digits
+% etc.
+
+% written by Prasanth Ganesan <pganesan2017@fau.edu>
+% file created on 05/18/2018
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+num = [9 8 7 5];
+result = [];
+k=2;
+while 1
+    sorted = [sort(num,2,'descend'); sort(num,2,'ascend')];
+    joined_num=[];
+    for temp=1:size(sorted,1)
+        strtemp = num2str(sorted(temp,:));
+        joined_num(temp,:) = str2double(strtemp(strtemp~=' '));
+    end
+    result(k) = max(joined_num)-min(joined_num);
+   
+    %disp(result(2:end))
+    if isequal(result(k),result(k-1))
+        disp(['Iterations: ' num2str(k)])
+        disp(['Final Number: ' num2str(result(k))])
+        break
+    end
+    num = str2double(regexp(num2str(result(k)),'\d','match'));
+    k=k+1;
+    if k > 10000
+        disp('Iterations exceeded maximum')
+        break
+    end
+end
